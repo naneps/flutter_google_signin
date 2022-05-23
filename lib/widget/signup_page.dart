7f558f327a04872e227d24b61c2143ede9ba5d7e
naneps <ekaprasetya2244@gmail.com>
2022-05-23 09:59:59 +0700

    create form

--- lib/widget/signup_page.dart
+++ lib/widget/signup_page.dart
@@ -1,24 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_google_signin/provider/google_sign_in.dart';
+import 'package:flutter_google_signin/widget/texf_field_widget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({Key? key}) : super(key: key);
+class SignUpPage extends StatefulWidget {
+  SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool showPass = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
           // crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Sign Up Page'),
+            Text(
+              'Sign Up',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            MyTextField(
+              icon: Icons.email,
+              hintText: "Email",
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            MyTextField(
+              showPass: showPass,
+              suffixIcon: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    showPass = !showPass;
+                  });
+                  // print(showPass);
+                },
+                child: Icon(showPass ? Icons.visibility : Icons.visibility_off),
+              ),
+              icon: FontAwesomeIcons.lock,
+              // showPass: ,
+              hintText: "Password",
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text("OR"),
+            SizedBox(
+              height: 15,
+            ),
             ElevatedButton.icon(
                 style: ElevatedButton.styleFrom(
                     primary: Colors.white,
